acd4762f3a894d8cb90a93674189ebd733901df8
brianamaftei <117312269+brianamaftei@users.noreply.github.com>
2023-03-14 19:27:28 +0200

    Add files via upload

---
+++ Forward engineering/CasaDeAsigurari.java
@@ -0,0 +1,14 @@
+import java.util.Vector;
+
+public class CasaDeAsigurari {
+
+  private String beneficiar;
+
+  private Integer suma;
+
+  private String platitor;
+
+    public Vector  myCabinet;
+    public Vector  myAnimal;
+
+}
---
+++ Forward engineering/ContUtilizator.java
@@ -0,0 +1,27 @@
+public class ContUtilizator extends Persoana {
+
+  private String ID;
+
+  private String parola;
+
+  private String email;
+
+  public void logOut() {
+  }
+
+  public void logIn(String ID, String parola) {
+  }
+
+  public void setParola(String parolaNoua) {
+  }
+
+  public void setID(String IDNou) {
+  }
+
+  public void setEmail(String emailNou) {
+  }
+
+  public void setAdresa(String adresaNoua) {
+  }
+
+}
